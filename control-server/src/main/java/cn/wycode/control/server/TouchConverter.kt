734ab5cbbdbe5e2602b70b2f7d659dd89cfc2206
wangyu <wangyu@wycode.cn>
2020-04-12 21:32:00 +0800

    修复id问题

--- control-server/src/main/java/cn/wycode/control/server/TouchConverter.kt
+++ control-server/src/main/java/cn/wycode/control/server/TouchConverter.kt
@@ -4,10 +4,11 @@ import android.os.SystemClock
 import android.view.InputDevice
 import android.view.MotionEvent
 import cn.wycode.control.common.HEAD_TOUCH_DOWN
-import cn.wycode.control.common.HEAD_TOUCH_MOVE
 import cn.wycode.control.common.HEAD_TOUCH_UP
+import cn.wycode.control.server.model.Point
 import cn.wycode.control.server.utils.Ln
-import java.util.*
+import cn.wycode.control.server.utils.PointersState
+
 
 const val MAX_POINTERS = 10
 
@@ -17,9 +18,8 @@ class TouchConverter {
     private val pointerProperties = arrayOfNulls<MotionEvent.PointerProperties>(MAX_POINTERS)
     private val pointerCoords = arrayOfNulls<MotionEvent.PointerCoords>(MAX_POINTERS)
 
-    private val pointerIdsPool = LinkedList<Int>()
+    private val pointersState = PointersState()
 
-    private val inputIdToPointerIdMap = LinkedHashMap<Byte, Int>()
 
     init {
         for (i in 0 until MAX_POINTERS) {
@@ -30,90 +30,40 @@ class TouchConverter {
             coords.size = 128f
             pointerProperties[i] = props
             pointerCoords[i] = coords
-            // 0 is default touch(first touch), second touch should start at 1
-            pointerIdsPool.add((i + 1))
         }
-        pointerIdsPool.removeLast()
     }
 
     fun convert(input: Event): MotionEvent? {
         val now = SystemClock.uptimeMillis()
 
-        val action: Int
-        var touchCount = inputIdToPointerIdMap.size
-        when (input.type) {
-            HEAD_TOUCH_DOWN -> {
-                lastTouchDown = now
-                if (pointerIdsPool.size == 0) {
-                    Ln.w("pointerIdsPool is empty!")
-                    return null
-                }
-                val pointerId: Int
-                when {
-                    touchCount == 0 -> {
-                        action = MotionEvent.ACTION_DOWN
-                        pointerId = 0
-                    }
-                    inputIdToPointerIdMap[input.id] != null -> {
-                        pointerId = inputIdToPointerIdMap[input.id]!!
-                        action =
-                            MotionEvent.ACTION_POINTER_DOWN or (pointerId shl MotionEvent.ACTION_POINTER_INDEX_SHIFT)
-                    }
-                    else -> {
-                        pointerId = pointerIdsPool.removeFirst()
-                        action =
-                            MotionEvent.ACTION_POINTER_DOWN or (pointerId shl MotionEvent.ACTION_POINTER_INDEX_SHIFT)
-                    }
-                }
-                inputIdToPointerIdMap[input.id] = pointerId
-                pointerProperties[touchCount]!!.id = pointerId
-                pointerCoords[touchCount]!!.x = input.x.toFloat()
-                pointerCoords[touchCount]!!.y = input.y.toFloat()
-                touchCount++
-            }
-            HEAD_TOUCH_MOVE -> {
-                //no record, drop the event
-                if (!inputIdToPointerIdMap.containsKey(input.id)) return null
-                action = MotionEvent.ACTION_MOVE
-                var i = 0 //the index of element need to update
-                for (entry in inputIdToPointerIdMap.entries) {
-                    if (entry.key == input.id) {
-                        pointerProperties[i]!!.id = entry.value
-                        pointerCoords[i]!!.x = input.x.toFloat()
-                        pointerCoords[i]!!.y = input.y.toFloat()
-                        break
-                    }
-                    i++
-                }
-            }
-            HEAD_TOUCH_UP -> {
-                // no record, drop the event
-                if (touchCount == 0 || !inputIdToPointerIdMap.containsKey(input.id)) return null
-                action = if (touchCount == 1) {
-                    MotionEvent.ACTION_UP
-                } else {
-                    MotionEvent.ACTION_POINTER_UP or (inputIdToPointerIdMap[input.id]!! shl MotionEvent.ACTION_POINTER_INDEX_SHIFT)
-                }
-                var i = 0 //the index of element need to update
-                val iterator = inputIdToPointerIdMap.entries.iterator()
-                while (iterator.hasNext()) {
-                    val entry = iterator.next()
-                    if (entry.key == input.id) {
-                        pointerProperties[i]!!.id = entry.value
-                        pointerCoords[i]!!.x = input.x.toFloat()
-                        pointerCoords[i]!!.y = input.y.toFloat()
-                        //recycle pointerId
-                        if (entry.value != 0) pointerIdsPool.addFirst(entry.value)
-                        //remove the id map
-                        iterator.remove()
-                        break
-                    }
-                    i++
-                }
+        val pointerIndex: Int = pointersState.getPointerIndex(input.id.toLong())
+        if (pointerIndex == -1) {
+            Ln.w("Too many pointers for touch event");
+            return null
+        }
 
+        val pointer = pointersState[pointerIndex]
+        pointer.point = Point(input.x, input.y)
+        pointer.pressure = 128f
+        pointer.isUp = input.type == HEAD_TOUCH_UP
+
+        val pointerCount = pointersState.update(pointerProperties, pointerCoords)
+
+        var action: Int = when (input.type) {
+            HEAD_TOUCH_DOWN -> MotionEvent.ACTION_DOWN
+            HEAD_TOUCH_UP -> MotionEvent.ACTION_UP
+            else -> MotionEvent.ACTION_MOVE
+        }
+        if (pointerCount == 1) {
+            if (input.type == HEAD_TOUCH_DOWN) {
+                lastTouchDown = now
             }
-            else -> {
-                return null
+        } else {
+            // secondary pointers must use ACTION_POINTER_* ORed with the pointerIndex
+            if (input.type == HEAD_TOUCH_UP) {
+                action = MotionEvent.ACTION_POINTER_UP or (pointerIndex shl MotionEvent.ACTION_POINTER_INDEX_SHIFT)
+            } else if (input.type == HEAD_TOUCH_DOWN) {
+                action = MotionEvent.ACTION_POINTER_DOWN or (pointerIndex shl MotionEvent.ACTION_POINTER_INDEX_SHIFT)
             }
         }
 
@@ -121,7 +71,7 @@ class TouchConverter {
             lastTouchDown,
             now,
             action,
-            touchCount,
+            pointerCount,
             pointerProperties,
             pointerCoords,
             0,
@@ -133,7 +83,6 @@ class TouchConverter {
             InputDevice.SOURCE_TOUCHSCREEN,
             0
         )
-        Ln.d("injectEvent::input->${input},inject->${event},touchCount=${touchCount},pointerIdsPoolwwpointerId=${pointerIdsPool}")
         return event
     }
 }

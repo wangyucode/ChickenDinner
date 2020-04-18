6513bc384819e34280dd32f79fe339a7a021f958
wangyu <wangyu@wycode.cn>
2020-04-18 19:36:38 +0800

    灵敏度配置

--- app/src/main/java/cn/wycode/control/MainActivity.kt
+++ app/src/main/java/cn/wycode/control/MainActivity.kt
@@ -2,7 +2,6 @@ package cn.wycode.control
 
 import android.app.Activity
 import android.content.Intent
-import android.content.res.Configuration
 import android.net.Uri
 import android.os.Bundle
 import android.provider.Settings

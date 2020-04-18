15ca22c090cfd875e89c3d449588e4adf52ca5bd
wangyu <wangyu@wycode.cn>
2020-04-18 17:04:58 +0800

    异形屏适配

--- app/src/main/java/cn/wycode/control/MainActivity.kt
+++ app/src/main/java/cn/wycode/control/MainActivity.kt
@@ -2,24 +2,41 @@ package cn.wycode.control
 
 import android.app.Activity
 import android.content.Intent
+import android.content.res.Configuration
 import android.net.Uri
 import android.os.Bundle
 import android.provider.Settings
+import android.view.LayoutInflater
+import android.view.View
 import android.widget.Toast
+
 const val OVERLAY_PERMISSION_REQUEST_CODE = 300
 
 class MainActivity : Activity() {
 
+    private lateinit var content: View
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        content = LayoutInflater.from(this).inflate(R.layout.activity_main, null)
+        setContentView(content)
         if (!Settings.canDrawOverlays(this)) {
             val intent = Intent()
             intent.action = Settings.ACTION_MANAGE_OVERLAY_PERMISSION
             intent.data = Uri.parse("package:$packageName")
             startActivityForResult(intent, OVERLAY_PERMISSION_REQUEST_CODE)
         }
+    }
+
+    override fun onConfigurationChanged(newConfig: Configuration) {
+        super.onConfigurationChanged(newConfig)
+        content.systemUiVisibility = (
+                View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+                        or View.SYSTEM_UI_FLAG_FULLSCREEN
+                        or View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                        or View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                        or View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                )
     }
 
     override fun onStart() {

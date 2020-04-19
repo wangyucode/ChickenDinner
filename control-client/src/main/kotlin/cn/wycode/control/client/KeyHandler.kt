b2f94d75dbfc46f5a2dd0914517cb1813e387cfd
wangyu <wangyu@wycode.cn>
2020-04-19 16:00:19 +0800

    配置调整

--- control-client/src/main/kotlin/cn/wycode/control/client/KeyHandler.kt
+++ control-client/src/main/kotlin/cn/wycode/control/client/KeyHandler.kt
@@ -155,7 +155,7 @@ class KeyHandler(private val connections: Connections) : EventHandler<KeyEvent> 
                         }
                         KEY_NAME_BAG -> if (!connections.mouseVisible){
                             fovHandler.stop()
-                            connections.sendBagOpen(Position(2103, 359))
+                            connections.sendBagOpen(Position(2691, 267))
                         }
                         KEY_NAME_ONE, KEY_NAME_TWO, KEY_NAME_THREE -> {
                             val index = buttonWithId.button.name!!.toInt()
